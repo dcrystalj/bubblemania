d5f2e25ce1a29a32d2f2fa501d5a70ab4b48af25
dcrystalj <dcrystalj@gmail.com>
2012-12-09 20:04:08 +0100

    added music file

---
+++ src/sound/Audioapp.java
@@ -0,0 +1,32 @@
+package sound;
+
+import java.io.*;
+import javax.sound.sampled.*;
+   
+// To play sound using Clip, the process need to be alive.
+// Hence, we use a Swing application.
+public class Audioapp {
+   public String[] music = new String []{"balloonpop.wav","gangnam.wav"};
+   // Constructor
+   public Audioapp(int i) {
+   
+      try {
+         // Open an audio input stream.
+    	 File f = new File(music[i]);
+		 AudioInputStream audioIn = AudioSystem.getAudioInputStream(f);// Get a sound clip resource.
+         Clip clip = AudioSystem.getClip();
+         // Open audio clip and load samples from the audio input stream.
+         clip.open(audioIn);
+         clip.start();
+         if(i==1)
+        	 clip.loop(1000);
+      } catch (UnsupportedAudioFileException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      } catch (LineUnavailableException e) {
+         e.printStackTrace();
+      }
+   }
+   
+}
